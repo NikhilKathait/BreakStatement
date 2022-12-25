385bc90bc9ff61a9353a05af82ad45f29ed9fb6f
Nikhil Kathait <nknick97@gmail.com>
2022-12-25 16:50:53 +0530

    Add project files.

---
+++ BreakStatement/BreakStatement.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        for(int i=0; i<10; i++)
+        {
+            
+            if(i==6)
+            {
+                System.Console.WriteLine("Break Statement");
+                break;
+            }
+            System.Console.WriteLine(i);
+        }
+    }
+}
